c1e2fcc849fe99bf6f2a0909e5aa0aa6c7d46444
tranvietduy06d4 <tranvietduy06d4@gmail.com>
2023-08-03 17:13:31 +0700

    Nop bai tap module 4 -ss14

---
+++ Module 4/ss14/thuc_hanh/demoSpringSecurity/src/main/java/com/example/repository/AppUserRepository.java
@@ -0,0 +1,12 @@
+package com.example.repository;
+
+import com.example.entity.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
+    AppUser findByUserName(String userName);
+
+
+}
